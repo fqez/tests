d35c5ced56642d6996eab7995a1d668f8086766f
Francisco Perez <f.perez475@gmail.com>
2020-01-14 10:38:39 +0100

    first release

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ if os.path.isfile(requirementPath):
         install_requires = f.read().splitlines()
         
 MAJOR               = 0
-MINOR               = 4
+MINOR               = 1
 MICRO               = 0
 ISRELEASED          = False
 VERSION             = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
