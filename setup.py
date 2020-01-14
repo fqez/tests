77a2ad72b4d69dc049c603d0ef9d3324b7a785b1
Francisco Pérez <f.perez475@gmail.com>
2020-01-14 08:53:48 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+import os
+thelibFolder = os.path.dirname(os.path.realpath(__file__))
+requirementPath = thelibFolder + '/requirements.txt'
+install_requires = [] # Examples: ["gunicorn", "docutils>=0.3", "lxml==0.5a7"]
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
+
+setup(name='testsfqez',
+      version='0.1',
+      description="mysetup",
+      url="https://github.com/fqez/tests",
+      packages=find_packages(exclude='test'),
+      include_package_data=True,
+      install_requires=install_requires,
+      zip_safe=True)
