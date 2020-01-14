d2b59801f50c57f435ca9ddd0dc64d43f418b7be
Francisco Pérez <f.perez475@gmail.com>
2020-01-14 10:01:06 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,15 @@ install_requires = [] # Examples: ["gunicorn", "docutils>=0.3", "lxml==0.5a7"]
 if os.path.isfile(requirementPath):
     with open(requirementPath) as f:
         install_requires = f.read().splitlines()
+        
+MAJOR               = 1
+MINOR               = 19
+MICRO               = 0
+ISRELEASED          = False
+VERSION             = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 setup(name='testsfqez',
-      version='0.1.0',
+      version=VERSION,
       description="mysetup",
       url="https://github.com/fqez/tests",
       packages=find_packages(exclude='test'),
