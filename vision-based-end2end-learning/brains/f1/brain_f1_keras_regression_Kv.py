6d540b39382202c407c2337165c4223d7fc88e2c
Francisco Perez <f.perez475@gmail.com>
2020-03-14 10:39:36 +0100

    add nb

---
+++ vision-based-end2end-learning/brains/f1/brain_f1_keras_regression_Kv.py
@@ -0,0 +1,40 @@
+"""
+    Robot: F1
+    Framework: keras
+    Number of networks: 1
+    Network type: Regression
+    Predicionts:
+        angular speed(w)
+
+    This brain uses a regression network based on Keras framework to predict the angular velocity of the F1 car.
+    For that task it uses a regression convolutional neural network for w leaving the linear speed constant
+"""
+
+import brains
+from behaviorlib.keras.keras_predict import KerasPredictor
+
+
+class Brain(brains.Brains):
+
+    def __init__(self, sensors, actuators):
+        super().__init__(sensors, actuators)
+        self.cont = 0
+        self.net_w = KerasPredictor('path_to_w')
+        self.k_v = 5
+        self.motors = self.get_motors('motors_0')
+
+    def load_brain(self, path):
+        raise AttributeError("Brain object has no attribute 'load_brain'")
+
+    def execute(self):
+        image = self.get_image('camera_0')
+
+        if self.cont > 0:
+            print("Runing...")
+            self.cont += 1
+
+        prediction_w = self.net_w.predict(image)
+
+        if prediction_w != '' and prediction_w != '':
+            self.motors.sendV(self.k_v)
+            self.motors.sendW(prediction_w)
